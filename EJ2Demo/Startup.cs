976e4e9e61307d70ec91d896bceb39e23b1bcc5a
kmuthukumar3032 <mkdmuthu@gmail.com>
2018-10-04 17:55:02 +0530

    initial commit

---
+++ EJ2Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EJ2Demo.Startup))]
+namespace EJ2Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ EJ2Demo/webpack.config.js
@@ -0,0 +1,18 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+
+const ROOT = path.resolve(__dirname, 'app');
+const DESTINATION = path.resolve(__dirname, 'Scripts');
+
+module.exports = {
+    context: ROOT,
+
+    entry: {
+        'index': './index.js'
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: DESTINATION
+    }	
+};
